32ec99e1de45fe4e4bd5dd9128a1153a31088166
newman-csa <146094244+newman-csa@users.noreply.github.com>
2023-11-20 19:25:09 +0000

    Changed call to SpriteBatch to Boot

--- core/src/cloud/game/sprites/FloatingCloud.java
+++ core/src/cloud/game/sprites/FloatingCloud.java
@@ -3,7 +3,6 @@ package cloud.game.sprites;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
@@ -12,10 +11,14 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.utils.Array;
 
 public class FloatingCloud {
+    private final Boot boot;
     private Texture flyingEnemyTexture;
     private Sound attackSound;
-    private SpriteBatch batch;
     private Array<Rectangle> Enemies;
+
+    public FloatingCloud(Boot boot) {
+        this.boot = boot // You can call batch and other redundant object from boot 
+    }
 
     private void moveEnemy(){}
 }
