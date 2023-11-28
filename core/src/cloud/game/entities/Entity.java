34586ca7af1d5733bfdefc0120e7b9262c0518fe
newman-csa <logann24@nycstudents.net>
2023-11-28 09:30:54 -0500

    Working on abstraction of entities

--- core/src/cloud/game/entities/Entity.java
+++ core/src/cloud/game/entities/Entity.java
@@ -3,12 +3,23 @@ package cloud.game.entities;
 import com.badlogic.gdx.physics.box2d.Body;
 
 public abstract class Entity {
-    protected float xCoord;
-    protected float yCoord;
-    protected float width;
-    protected float height;
+    protected float xCoord, yCoord, width, height;
+    protected float xVel, yVel, speedRate;
     protected Body body;
 
-    public void setup() {}
-    public void update() {}
+    public Entity(Body body, float width, float height, float xCoord, float yCoord) {
+        this.body = body;
+        this.width = width;
+        this.height = height;
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+
+        // From 1 to 0
+        this.xVel = 0;
+        this.yVel = 0;
+        this.speedRate = 0;
+    }
+    public abstract void update();
+    public abstract void render();
+
 }
--- core/src/cloud/game/entities/Player.java
+++ core/src/cloud/game/entities/Player.java
@@ -1,5 +1,33 @@
 package cloud.game.entities;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 public class Player extends Entity {
 
+    public Player() {
+        // https://www.geeksforgeeks.org/difference-between-super-and-super-in-java-with-examples/
+        super();
+
+    }
+
+    @Override
+    public void update() {
+        if(Gdx.input.isKeyPressed(Input.Keys.D)) {
+
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.A)) {
+
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.W)) {
+
+        }
+    }
+
+    @Override
+    public void render() {
+
+    }
 }
