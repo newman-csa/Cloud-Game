628b0151c7bb2a951e8d5e46cdc48723298584e6
newman-csa <logann24@nycstudents.net>
2023-12-02 20:44:28 -0500

    Refactored

--- core/src/cloud/game/Boot.java
+++ core/src/cloud/game/Boot.java
@@ -1,16 +1,22 @@
 package cloud.game;
 
-
 import cloud.game.levels.Level1;
+import cloud.utils.AssetsLoader;
 import com.badlogic.gdx.Game;
-
-
+import com.badlogic.gdx.graphics.OrthographicCamera;
 
 public class Boot extends Game {
+	public OrthographicCamera camera;
+	public AssetsLoader assetsLoader;
 
 	@Override
 	public void create () {
-		this.setScreen(new Level1());
+		camera = new OrthographicCamera();
+		assetsLoader = new AssetsLoader();
+
+		camera.setToOrtho(false, 32f , 18f);
+
+		this.setScreen(new Level1(this));
 	}
 
 	@Override
@@ -20,6 +26,7 @@ public class Boot extends Game {
 
 	@Override
 	public void dispose () {
+		assetsLoader.assetManager.dispose();
 
 	}
 }
