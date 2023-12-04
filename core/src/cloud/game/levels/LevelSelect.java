5424ded1789dd7bc2031f035aa1fb234c8425828
Peterr134 <peterr134@nycstudents.net>
2023-12-03 23:21:29 -0500

    Added buttons for levels 2 and 3, and set up directs so they change screens, but they all go to level 1 for now

--- core/src/cloud/game/levels/LevelSelect.java
+++ core/src/cloud/game/levels/LevelSelect.java
@@ -27,6 +27,13 @@ public class LevelSelect implements Screen {
         boot.batch.begin();
         boot.font12.draw(boot.batch, "Level 1", 100, 350);
         Rectangle level1Rect = new Rectangle("Level 1".length()*30,200,150,50);
+
+        boot.font12.draw(boot.batch, "Level 2", 350, 350);
+        Rectangle level2Rect = new Rectangle(600,200,"Level 2".length()*20,50);
+
+        boot.font12.draw(boot.batch, "Level 3", 600, 350);
+        Rectangle level3Rect = new Rectangle(1000,200,"Level 3".length()*20,50);
+
         //sorry I gave up on tables
         //rectangles are the only thing I know
         //all is rectangles
@@ -35,6 +42,15 @@ public class LevelSelect implements Screen {
         Rectangle mouseRect = new Rectangle(Gdx.input.getX(), Gdx.input.getY(), 1, 1);
 
         if(Gdx.input.isTouched() && mouseRect.overlaps(level1Rect)) {
+            //goes to level 1
+            boot.setScreen(new Level1(boot));
+            dispose();
+        }else if(Gdx.input.isTouched() && mouseRect.overlaps(level2Rect)) {
+            //goes to level 2
+            boot.setScreen(new Level1(boot));
+            dispose();
+        }else if(Gdx.input.isTouched() && mouseRect.overlaps(level3Rect)) {
+            //goes to level 3
             boot.setScreen(new Level1(boot));
             dispose();
         }
